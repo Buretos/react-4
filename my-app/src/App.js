efe51f134c4a70dde42c8887dd78a9e186838411
Aleksandr Morozov <moroz.cuporos@gmail.com>
2023-12-09 13:12:25 +0300

    Lesson 5-1, video 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,17 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-	const [products, setProducts] = useState([]);
+	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
-		fetch('https://mocki.io/v1/49741d30-63a8-4662-9e80-fc4f949d2758')
-			.then((loadedData) => loadedData.json())
-			.then((loadedProducts) => {
-				setProducts(loadedProducts);
-			});
-	}, []);
+		console.log('Первый коллбэк -', counter);
+
+		return () => console.log('Второй коллбэк -', counter);
+	}, [counter]);
 
 	return (
 		<div className={styles.app}>
-			{products.map(({ id, name, price }) => (
-				<div key={id}>
-					{name} - {price} руб.
-				</div>
-			))}
+			<button onClick={() => setCounter(counter + 1)}>+ 1</button>
 		</div>
 	);
 };
