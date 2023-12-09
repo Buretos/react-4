e23655eb0ab27b59f2f43c0cd40815096bc7743f
Aleksandr Morozov <moroz.cuporos@gmail.com>
2023-12-09 18:16:57 +0300

    Lesson-5-4, video-1, JSON server PUT, PATCH, DELETE

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ export const App = () => {
 	const [products, setProducts] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [isCreating, setIsCreating] = useState(false);
+	const [isUpdating, setIsUpdating] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
 	const [refreshProductsFlag, setRefreshProductsFlag] = useState(false);
 
 	const refreshProducts = () => setRefreshProductsFlag(!refreshProductsFlag);
@@ -38,6 +40,37 @@ export const App = () => {
 			.finally(() => setIsCreating(false));
 	};
 
+	const requestUpdateSmartphone = () => {
+		setIsUpdating(true);
+		fetch('http://localhost:3005/produckts/002', {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				name: 'Сматрфон',
+				price: 17900,
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				console.log('Смартфон обновлён, ответ сервера:', response);
+				refreshProducts();
+			})
+			.finally(() => setIsUpdating(false));
+	};
+
+	const requestDeleteHairDryer = () => {
+		setIsDeleting(true);
+		fetch('http://localhost:3005/produckts/003', {
+			method: 'DELETE',
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				console.log('Фен удалён, ответ сервера:', response);
+				refreshProducts();
+			})
+			.finally(() => setIsUpdating(false));
+	};
+
 	return (
 		<div className={styles.app}>
 			{isLoading ? (
@@ -51,6 +84,12 @@ export const App = () => {
 			)}
 			<button disabled={isCreating} onClick={requestAddVacuumCleaner}>
 				Добавить пылесос
+			</button>
+			<button disabled={isUpdating} onClick={requestUpdateSmartphone}>
+				Обновить смартфон
+			</button>
+			<button disabled={isDeleting} onClick={requestDeleteHairDryer}>
+				Удалить фен
 			</button>
 		</div>
 	);
